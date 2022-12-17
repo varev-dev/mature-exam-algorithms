86f4630a4776bc56615bd0735e92f19eddb2f2fe
Kacper Doga <doga.kacper@gmail.com>
2022-12-17 23:24:52 +0100

    implemented palindrome check algorithm

---
+++ matura-algorithms/src/main/java/TextAlgorithm.java
@@ -0,0 +1,33 @@
+public class TextAlgorithm {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("Kamil slimaka"));
+    }
+
+    public static boolean isPalindrome(String text) {
+        StringBuilder builder = new StringBuilder(cleanText(text));
+
+        return cleanText(text).equals(builder.reverse().toString());
+    }
+
+    private static String cleanText(String text) {
+        StringBuilder output = new StringBuilder();
+        text = text.toUpperCase();
+        int charId = 0;
+
+        while (charId != text.length()) {
+            if (isInAlphabet(text.toCharArray()[charId]))
+                output.append(text.toCharArray()[charId]);
+
+            charId++;
+        }
+
+
+        return output.toString();
+    }
+
+    private static boolean isInAlphabet(char characterToCheck) {
+        return (int) characterToCheck <= (int) 'Z' && (int) characterToCheck >= (int) 'A';
+    }
+
+}
