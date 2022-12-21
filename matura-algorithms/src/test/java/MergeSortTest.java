c6f42cbe84d64f285e760876e1cb6c218693914d
Kacper Doga <doga.kacper@gmail.com>
2022-12-21 23:37:08 +0100

    created tests for merge sort
    - empty array
    - array contains only same elements

--- matura-algorithms/src/test/java/MergeSortTest.java
+++ matura-algorithms/src/test/java/MergeSortTest.java
@@ -6,6 +6,19 @@ import java.util.Arrays;
 public class MergeSortTest {
 
     @Test
+    public void testMergeSortEmptyArray() {
+        // Given
+        int[] input = new int[]{};
+
+        // When
+        int[] expectedOutput = new int[]{};
+        boolean output = Arrays.equals(expectedOutput, SortAlgorithm.mergeSort(input));
+
+        // Then
+        Assert.assertTrue(output);
+    }
+
+    @Test
     public void testMergeSortOnSixElements() {
         // Given
         int[] input = new int[]{4, 1, 9, 5, 6, 8, 7};
@@ -13,6 +26,18 @@ public class MergeSortTest {
         // When
         int[] expectedOutput = new int[]{1, 4, 5, 6, 7, 8, 9};
         boolean output = Arrays.equals(SortAlgorithm.mergeSort(input), expectedOutput);
+
+        // Then
+        Assert.assertTrue(output);
+    }
+
+    @Test
+    public void testMergeSortOnSameElements() {
+        // Given
+        int[] input = new int[]{1, 1, 1, 1, 1};
+
+        // When
+        boolean output = Arrays.equals(input, SortAlgorithm.mergeSort(input));
 
         // Then
         Assert.assertTrue(output);
