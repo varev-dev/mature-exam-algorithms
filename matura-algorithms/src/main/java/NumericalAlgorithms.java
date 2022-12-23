39c5b720d16b97d1e8de93f39263fe7dd3fed4c8
Kacper Doga <doga.kacper@gmail.com>
2022-12-23 15:48:20 +0100

    implemented getting fibonacci sequence element

---
+++ matura-algorithms/src/main/java/NumericalAlgorithms.java
@@ -0,0 +1,13 @@
+public class NumericalAlgorithms {
+
+    public static int getFibonacciSequenceElement(int elementId) {
+        if (elementId == 0)
+            return 0;
+
+        if (elementId <= 2)
+            return 1;
+
+        return getFibonacciSequenceElement(elementId - 1) + getFibonacciSequenceElement(elementId - 2);
+    }
+
+}
