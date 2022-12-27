d29fe79583f6b5966e87d069e4081848a88da31a
varev-dev <KACDOGA4@edu.gdansk.pl>
2022-12-27 13:33:53 +0100

    refactored binary search implementation

--- matura-algorithms/src/main/java/SearchAlgorithm.java
+++ matura-algorithms/src/main/java/SearchAlgorithm.java
@@ -1,17 +1,10 @@
 public class SearchAlgorithm {
 
-    public static int linearSearch(int[] array, int value) {
-        return -1;
-    }
-
     public static int binarySearch(int[] array, int value, int beginning, int end) {
         if (array.length < 1 || beginning > end)
             return -1;
 
         int middleIndex = (beginning + end) / 2;
-
-        if (array.length == 1)
-            return array[middleIndex] == value ? array[middleIndex] : -1;
 
         if (array[middleIndex] == value)
             return middleIndex;
