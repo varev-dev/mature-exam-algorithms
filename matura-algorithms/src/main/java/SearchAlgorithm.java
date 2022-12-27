9ab70184d75d02ac00dcc0260f30689a20c67aca
varev-dev <KACDOGA4@edu.gdansk.pl>
2022-12-27 13:55:21 +0100

    implemented linear sentinel search

--- matura-algorithms/src/main/java/SearchAlgorithm.java
+++ matura-algorithms/src/main/java/SearchAlgorithm.java
@@ -1,5 +1,16 @@
 public class SearchAlgorithm {
 
+    public static int sentinelLinearSearch(int[] array, int value) {
+        int sentinel = Integer.MAX_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value)
+                return i;
+        }
+
+        return sentinel;
+    }
+
     public static int binarySearch(int[] array, int value, int beginning, int end) {
         if (array.length < 1 || beginning > end)
             return -1;
